38ad8dbd954ddc0957eb05d5eb9a458c05936bc6
Itsrazzx <330963@student.mboutrecht.nl>
2021-04-14 01:13:56 +0200

    Timer toegevoegd

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+
+    private float elapsedTime;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        timeCounter.text = "Time: 00:00.00";
+        BeginTimer();
+    }
+
+    public void BeginTimer()
+    {
+        timerGoing = true;
+        elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerGoing = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+
+            yield return null;
+        }
+    }
+}
